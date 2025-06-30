9d18fb21ce5f3b9ab6b55e31f08eb0f3da34bf05
Krish Bhollar Tyagi <krishbtyagi@gmail.com>
2025-06-30 12:58:09 +0530

    added routes skeleton

---
+++ routes/course.js
@@ -0,0 +1,21 @@
+const {Router} = require("express");
+const courseRouter = Router();
+
+
+courseRouter.get("/preview", function(req, res){
+    res.json({
+        message : "courses endpoint"
+    })
+})
+
+
+
+courseRouter.post("/purchase", function(req, res){
+    res.json({
+        message:"course purchasing endpoint"
+    })
+})
+
+module.exports = {
+    courseRouter: courseRouter
+}
