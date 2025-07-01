cb81c30030120ddf11140987ff8c99bcec5b4f7c
Krish Bhollar Tyagi <krishbtyagi@gmail.com>
2025-07-01 12:26:36 +0530

    added auth endpoints

--- routes/admin.js
+++ routes/admin.js
@@ -1,32 +1,67 @@
 const {Router} = require("express");
 const adminRouter = Router();
 const {adminModel} = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_ADMIN = "123admin321";
 
-adminRouter.post("signup", function(req,res){
+adminRouter.post("/signup", async function(req, res){
+    const {email, password, firstName, lastName } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try{
+        await adminModel.create({
+            email: email,
+            password : hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
     res.json({
-        message : "signup endpoint"
-    })
+        message : "signup succeeded"
+    })    
+    } catch(error){
+        res.json({message : "failed"});
+        console.error(error);
+    }
 })
 
-adminRouter.post("/signin", function(req,res){
+adminRouter.post("/signin", async function(req, res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const admin = await adminModel.findOne({
+        email: email
+    });
+
+    const passwordMatch = bcrypt.compare(password, admin.password);
+    if(admin && passwordMatch){
+        const token = jwt.sign({
+            id: admin._id.toString()
+        }, JWT_ADMIN,
+        { expiresIn : '1d'}
+    );
+        res.json({
+            token
+        })
+    }else {
+        res.status(403).json({
+            message : "incorrect creds"
+        })
+    }
+})
+
+adminRouter.post("/course", function(req, res){
     res.json({
         message : "signin endpoint"
     })
 })
 
-adminRouter.post("/course", function(req,res){
+adminRouter.put("/course", function(req, res){
     res.json({
         message : "signin endpoint"
     })
 })
 
-adminRouter.put("/course", function(req,res){
-    res.json({
-        message : "signin endpoint"
-    })
-})
-
-adminRouter.get("/course/bulk", function(req,res){
+adminRouter.get("/course/bulk", function(req, res){
     res.json({
         message : "signin endpoint"
     })
--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,54 @@
 const {Router} = require("express");
-
+const {userModel} = require("../db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const JWT_USER = "123user321";
 const userRouter = Router();
 
-userRouter.post("signup", function(req,res){
+userRouter.post("/signup", async function(req,res){
+    const {email, password, firstName, lastName } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    try{
+        await userModel.create({
+            email: email,
+            password : hashedPassword,
+            firstName: firstName,
+            lastName: lastName
+        })
     res.json({
-        message : "signup endpoint"
-    })
+        message : "signup succeeded"
+    })    
+    } catch(error){
+        res.json({message : "failed"});
+        console.error(error);
+    }
+    
 })
  
-userRouter.post("/signin", function(req,res){
-    res.json({
-        message : "signin endpoint"
-    })
-})
+userRouter.post("/signin", async function(req,res){
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await userModel.findOne({
+        email: email
+    });
+
+    const passwordMatch = bcrypt.compare(password, user.password);
+    if(user && passwordMatch){
+        const token = jwt.sign({
+            id: user._id.toString()
+        }, JWT_USER,
+        { expiresIn : '1d'}
+    );
+        res.json({
+            token
+        })
+    }else {
+        res.status(403).json({
+            message : "incorrect creds"
+        })
+    }
+});
 
 userRouter.get("/purchases", function(req,res){
     res.json({
